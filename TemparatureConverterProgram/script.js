6babeee53e237a9dafbbe2c42b45006a09d4a7d6
Eric-Gitau01 <gitaueric01@gmail.com>
2025-05-19 00:16:20 +0300

    created a temperature converter program

--- TemparatureConverterProgram/script.js
+++ TemparatureConverterProgram/script.js
@@ -1,7 +1,19 @@
-for(let i = 0; i <= 20; i++) {
-    if(i == 13) {
-        break
+const textBox = document.getElementById("textBox");
+const toFahrentit = document.getElementById("toFahrentit")
+const toCelsius = document.getElementById("toCelcius")
+const result = document.getElementById("result")
+let temp = ''
+
+function convert() {
+    if(toFahrentit.checked) {
+        temp = Number(textBox.value)
+        temp = temp * 9 / 5 + 32;
+        result.textContent = temp.toFixed(1) + "°F"
+    } else if(toCelsius.checked) {
+        temp = Number(textBox.value)
+        temp = (temp - 32) * (5 / 9)
+        result.textContent = temp.toFixed(1) + "°C"
     } else {
-        console.log(i)
+        result.textContent = "Select a unit"
     }
-} 
+}
